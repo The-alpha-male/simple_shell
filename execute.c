48feee5172cf9d75db754362cd6fac047980aad8
The-alpha-male <ronnymunene@outlook.com>
2023-09-14 14:58:15 -0700

    execute files added

---
+++ execute.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+#include <stdlib.h>
+#include <string.h>
+
+extern char **environment;
+
+/**
+ * exe_command - checks if 1st element of array is NULL
+ * @args: pointer to a args
+ * Return: (1) SUCCESS
+ */
+
+int exe_command(char **args)
+{
+	pid_t pid;
+	int status;
+
+	if (args[0] == NULL)
+	{
+		return (1);
+	}
+
+	if (is_built_in(args[0])
+			{
+			if strcmp(args[0], "exit") == 0
+			{
+				handle_exit();
+			} else if (strcmp(args[0], "env") == 0)
+			{
+				handle_environmnet();
+			}
+			return (1);
+	}
+
+	pid = fork();
+	if (pid == 0)
+	{
+
+		execvp(args[0], args);
+
+		perror("execvp");
+		exit(EXIT_FAILURE);
+	} else if (pid < 0)
+	{
+		perrors("fork");
+	} else
+	{
+		do {
+			status = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return (1);
