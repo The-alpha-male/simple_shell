424a32ec62d7b6f3e4294799c2b53ed21841c2c7
will-me-me <willykaria777@gmail.com>
2023-09-21 14:48:52 -0700

    update user input file

--- user_inputs.c
+++ user_inputs.c
@@ -4,5 +4,40 @@
 #include <unistd.h>
 #include "shell.h"
 /**
- * user
+ * userinput - tokenizing args
+ * @str: -str to be tokenized
+ * Return: - a pointer array
+ */
 char **userinput(char *str)
+{
+	char *args;
+	int i;
+	char tkns;
+	const char *delimitor = " \n\t\r";
+
+	i = 0;
+	args = malloc(sizeof(char *) * 300);
+
+	if (!args)
+		return (NULL);
+	tkns = strtok(str, delimitor);
+	while (tkns)
+	{
+		args[i] = strdup(tkns);
+		if (!args[i])
+		{
+			if (args)
+			{
+				for (i = 0; args[i] != NULL; i++)
+					free(args[i]);
+
+				free(args);
+			}
+			return (NULL);
+		}
+		tkns = strtok(NULL, delimitor);
+		i++
+	}
+	args[i] = NULL;
+	return (args);
+}
