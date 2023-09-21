64422a84252d0d9f5f97a8a56e4aa37c5b6fc4c0
will-me-me <willykaria777@gmail.com>
2023-09-20 23:23:33 -0700

    new prompmt

--- prompt.c
+++ prompt.c
@@ -1,5 +1,5 @@
 #include "shell.h"
-
+#include <unistd.h>
 /**
  * display_prompt - Display shell prompt.
  * Display shell pront
@@ -8,5 +8,7 @@
 void display_prompt(void)
 {
 	if (isatty(STDIN_FILENO))
-		write(STDOUT_FILEO, "$ ", 2)
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+	}
 }
