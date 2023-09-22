38ecbf2a80092844a69858b81183130358896bf5
The-alpha-male <ronnymunene@outlook.com>
2023-09-21 17:13:57 -0700

    changed output

--- prompt.c
+++ prompt.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 /**
  * display_prompt - Display shell prompt.
- * Display shell pront
+ * Display shell promt
  */
 
 void display_prompt(void)
 {
 	if (isatty(STDIN_FILENO))
 	{
-		write(STDOUT_FILENO, "$ ", 2);
+		write(STDOUT_FILENO, "WillRon $", 2);
 	}
 }
